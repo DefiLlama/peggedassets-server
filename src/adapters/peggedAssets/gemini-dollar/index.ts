f2a96bd68c55caf854d38ca796a3f692063af732
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-06-02 03:04:31 -0400

    add GUSD

---
+++ src/adapters/peggedAssets/gemini-dollar/index.ts
@@ -0,0 +1,69 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  bridgedSupply,
+} from "../helper/getSupply";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  ethereum: {
+    issued: ["0x056fd409e1d7a124bd7017459dfea2f387b6d5cd"],
+  },
+  wan: {
+    bridgedFromETH: ["0xcF422327dDaAa409C2976d01131d8a3457F03251"],
+  },
+};
+
+/*
+multichain version on Fantom but can't find contract.
+*/
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** decimals, "issued", false);
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  ethereum: {
+    minted: chainMinted("ethereum", 2),
+    unreleased: async () => ({}),
+  },
+  wan: {
+    minted: async () => ({}),
+    unreleased: async () => ({}),
+    ethereum: bridgedSupply(
+      "wan",
+      2,
+      chainContracts.wan.bridgedFromETH
+    ),
+  },
+};
+
+export default adapter;
