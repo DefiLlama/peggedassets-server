38a6c702c2d3b9416d9a8e15b15df598c3dddde2
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-06-07 14:21:24 -0400

    add missing alchemix file

---
+++ src/adapters/peggedAssets/alchemix-usd/index.ts
@@ -0,0 +1,89 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  bridgedSupply,
+} from "../helper/getSupply";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  ethereum: {
+    issued: ["0xbc6da0fe9ad5f3b0d58160288917aa56653660e9"],
+  },
+  arbitrum: {
+    bridgedFromETH: ["0x2130d2a1e51112D349cCF78D2a1EE65843ba36e0"], // multichain
+  },
+  optimism: {
+    bridgedFromETH: ["0xb2c22A9fb4FC02eb9D1d337655Ce079a04a526C7"], // multichain
+  },
+  fantom: {
+    bridgedFromETH: ["0xB67FA6deFCe4042070Eb1ae1511Dcd6dcc6a532E"], // has 1.5M more than in multichain bridge contract
+  }
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** decimals, "issued", false);
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  ethereum: {
+    minted: chainMinted("ethereum", 18),
+    unreleased: async () => ({}),
+  },
+  arbitrum: {
+    minted: async () => ({}),
+    unreleased: async () => ({}),
+    ethereum: bridgedSupply(
+      "arbitrum",
+      18,
+      chainContracts.arbitrum.bridgedFromETH
+    ),
+  },
+  optimism: {
+    minted: async () => ({}),
+    unreleased: async () => ({}),
+    ethereum: bridgedSupply(
+      "optimism",
+      18,
+      chainContracts.optimism.bridgedFromETH
+    ),
+  },
+  fantom: {
+    minted: async () => ({}),
+    unreleased: async () => ({}),
+    ethereum: bridgedSupply(
+      "fantom",
+      18,
+      chainContracts.fantom.bridgedFromETH
+    ),
+  },
+};
+
+export default adapter;
