2b60a9839f912b84db83d6b565a21ed7723177a4
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-07-03 04:11:39 -0400

    add timeout errors for adapters

--- src/peggedAssets/storePeggedAssets/storePegged.ts
+++ src/peggedAssets/storePeggedAssets/storePegged.ts
@@ -5,20 +5,32 @@ import { importAdapter } from "../utils/importAdapter";
 
 const maxRetries = 4;
 
+const timeout = (prom: any, time: number, peggedID: string) =>
+  Promise.race([prom, new Promise((_r, rej) => setTimeout(rej, time))]).catch(
+    (err) => {
+      console.error(`Could not store peggedAsset ${peggedID}`, err);
+    }
+  );
+
 async function iteratePeggedAssets(peggedIndexes: number[]) {
   const { timestamp, ethereumBlock, chainBlocks } = await getCurrentBlocks();
   const actions = peggedIndexes
     .map((idx) => peggedAssets[idx])
     .map((peggedAsset) => {
       const adapterModule = importAdapter(peggedAsset);
-      return storePeggedAsset(
-        timestamp,
-        ethereumBlock,
-        chainBlocks,
-        peggedAsset,
-        adapterModule,
-        maxRetries,
-        true
+      // times out after 60 seconds
+      return timeout(
+        storePeggedAsset(
+          timestamp,
+          ethereumBlock,
+          chainBlocks,
+          peggedAsset,
+          adapterModule,
+          maxRetries,
+          true
+        ),
+        60000,
+        peggedAsset.gecko_id
       );
     });
   await Promise.all(actions);
