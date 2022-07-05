6a3161c364f206dd56e31e27f204c3776947f55c
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-07-05 01:26:20 -0400

    Revert "new stablecoinChart endpoint format"
    This reverts commit 598b494be30c4c80ba460270b82988ae094796f8.
    
    revert

--- src/getStablecoinChart.ts
+++ src/getStablecoinChart.ts
@@ -59,13 +59,14 @@ export async function craftChartsResponse(
   chain: string | undefined,
   peggedAsset: string | undefined
 ) {
+  console.time("chart");
   const sumDailyBalances = {} as {
     [timestamp: number]: {
       circulating: tokenBalance;
       unreleased: tokenBalance;
-      circulatingMcap: tokenBalance;
-      mintedMcap: tokenBalance;
-      bridgedToMcap: tokenBalance;
+      bridgedTo: tokenBalance;
+      minted: tokenBalance;
+      mcap: number;
     };
   };
   // quick fix; need to update later
@@ -235,30 +236,27 @@ export async function craftChartsResponse(
               (sumDailyBalances[timestamp].circulating[pegType] ?? 0) +
               itemBalance.circulating[pegType];
 
-            sumDailyBalances[timestamp].unreleased =
+              sumDailyBalances[timestamp].unreleased =
               sumDailyBalances[timestamp].unreleased || {};
             sumDailyBalances[timestamp].unreleased[pegType] =
               (sumDailyBalances[timestamp].unreleased[pegType] ?? 0) +
               itemBalance.unreleased[pegType];
 
-            sumDailyBalances[timestamp].circulatingMcap =
-              sumDailyBalances[timestamp].circulatingMcap || {};
-            sumDailyBalances[timestamp].circulatingMcap[pegType] =
-              (sumDailyBalances[timestamp].circulatingMcap[pegType] ?? 0) +
+            sumDailyBalances[timestamp].mcap =
+              (sumDailyBalances[timestamp].mcap ?? 0) +
               itemBalance.circulating[pegType] * price;
 
-            sumDailyBalances[timestamp].mintedMcap =
-              sumDailyBalances[timestamp].mintedMcap || {};
-            sumDailyBalances[timestamp].mintedMcap[pegType] =
-              (sumDailyBalances[timestamp].mintedMcap[pegType] ?? 0) +
-              (itemBalance.minted[pegType] - itemBalance.unreleased[pegType]) *
-                price;
-
-            sumDailyBalances[timestamp].bridgedToMcap =
-              sumDailyBalances[timestamp].bridgedToMcap || {};
-            sumDailyBalances[timestamp].bridgedToMcap[pegType] =
-              (sumDailyBalances[timestamp].bridgedToMcap[pegType] ?? 0) +
+            sumDailyBalances[timestamp].bridgedTo =
+              sumDailyBalances[timestamp].bridgedTo || {};
+            sumDailyBalances[timestamp].bridgedTo[pegType] =
+              (sumDailyBalances[timestamp].bridgedTo[pegType] ?? 0) +
               itemBalance.bridgedTo[pegType] * price;
+
+            sumDailyBalances[timestamp].minted =
+              sumDailyBalances[timestamp].minted || {};
+            sumDailyBalances[timestamp].minted[pegType] =
+              (sumDailyBalances[timestamp].minted[pegType] ?? 0) +
+              itemBalance.minted[pegType] * price;
           } else {
             console.log(
               "itemBalance is invalid",
@@ -277,11 +275,11 @@ export async function craftChartsResponse(
   const response = Object.entries(sumDailyBalances).map(
     ([timestamp, balance]) => ({
       date: timestamp,
+      mcap: balance.mcap,
       totalCirculating: balance.circulating,
-      totalUnreleased: balance.unreleased,
-      totalCirculatingUSD: balance.circulatingMcap,
-      totalMintedUSD: balance.mintedMcap,
-      totalBridgedToUSD: balance.bridgedToMcap,
+      unreleased: balance.unreleased,
+      bridgedTo: balance.bridgedTo,
+      minted: balance.minted,
     })
   );
 
