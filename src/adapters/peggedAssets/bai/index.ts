3c7cdf96a6469144d683708bd877417975dc774c
jack-lvsjack <106633601+jack-lvsjack@users.noreply.github.com>
2022-07-06 02:35:27 +0800

    feat: add BAI adapter (#1)
    Signed-off-by: Jack Yin <lvsjack@qq.com>
    
    Co-authored-by: Jack Yin <lvsjack@qq.com>

---
+++ src/adapters/peggedAssets/bai/index.ts
@@ -0,0 +1,51 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  astar: {
+    issued: ["0x733ebcC6DF85f8266349DEFD0980f8Ced9B45f35"],
+  }
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** decimals);
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  avalanche: {
+    minted: chainMinted("astar", 18),
+    unreleased: async () => ({}),
+  }
+};
+
+export default adapter;
