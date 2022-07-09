2c6c9e63b107e81b4d7c6be8e5c543576c23cf72
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-07-09 08:31:20 -0400

    allow getBlocks to timeout when storing peggedAssets

--- src/peggedAssets/storePeggedAssets/storePegged.ts
+++ src/peggedAssets/storePeggedAssets/storePegged.ts
@@ -13,27 +13,33 @@ const timeout = (prom: any, time: number, peggedID: string) =>
   );
 
 async function iteratePeggedAssets(peggedIndexes: number[]) {
-  const { timestamp, ethereumBlock, chainBlocks } = await getCurrentBlocks();
-  const actions = peggedIndexes
-    .map((idx) => peggedAssets[idx])
-    .map((peggedAsset) => {
-      const adapterModule = importAdapter(peggedAsset);
-      // times out after 60 seconds
-      return timeout(
-        storePeggedAsset(
-          timestamp,
-          ethereumBlock,
-          chainBlocks,
-          peggedAsset,
-          adapterModule,
-          maxRetries,
-          true
-        ),
-        60000,
-        peggedAsset.gecko_id
-      );
-    });
-  await Promise.all(actions);
+  const { timestamp, ethereumBlock, chainBlocks } = await timeout(
+    getCurrentBlocks(),
+    60000,
+    "get blocks"
+  );
+  if (timestamp) {
+    const actions = peggedIndexes
+      .map((idx) => peggedAssets[idx])
+      .map((peggedAsset) => {
+        const adapterModule = importAdapter(peggedAsset);
+        // times out after 60 seconds
+        return timeout(
+          storePeggedAsset(
+            timestamp,
+            ethereumBlock,
+            chainBlocks,
+            peggedAsset,
+            adapterModule,
+            maxRetries,
+            true
+          ),
+          60000,
+          peggedAsset.gecko_id
+        );
+      });
+    await Promise.all(actions);
+  }
   return;
 }
 
