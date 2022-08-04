7dfceb5eef82cf410efb2f19291e9f423016ab3a
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-08-04 02:33:17 -0400

    add ontology helper

---
+++ src/adapters/peggedAssets/helper/ontology.ts
@@ -0,0 +1,33 @@
+const axios = require("axios");
+const retry = require("async-retry");
+const BigNumber = require("bignumber.js");
+
+export async function getTotalSupply(address: string, tokenType: string) {
+  const supplyRes = await retry(
+    async (_bail: any) =>
+      await axios.get(
+        `https://explorer.ont.io/v2/tokens/${tokenType}/${address}`
+      )
+  );
+
+  const bnSupply = new BigNumber(supplyRes?.data?.result?.total_supply);
+
+  return bnSupply.toNumber();
+}
+
+export async function getBalance(address: string, tokenType: string, owner: string) {
+  const balancesRes = await retry(
+    async (_bail: any) =>
+      await axios.get(
+        `https://explorer.ont.io/v2/addresses/${owner}/${tokenType}/balances`
+      )
+  );
+
+  const filteredBalances = balancesRes?.data?.result?.filter(
+    (balance: any) => balance.contract_hash === address
+  );
+
+  const bnBalance = new BigNumber(filteredBalances[0]?.balance);
+
+  return bnBalance.toNumber();
+}
