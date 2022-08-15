84740467e24630f39ab69f10b14b801b587fe89d
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-08-15 13:27:23 -0400

    delete files leftover from defillama-server fork

--- src/cli/findUnupdated.ts
+++
@@ -1,8 +0,0 @@
-import findOutdated from "../utils/findOutdated";
-
-const maxDrift = 3 * 3600; // Max three updates missed
-
-async function main() {
-  console.log(await findOutdated(maxDrift));
-}
-main();
--- src/utils/imports/importAdapter.ts
+++
@@ -1,6 +0,0 @@
-import { Protocol } from "../../protocols/types";
-import adapters from "./adapters";
-
-export function importAdapter(protocol: Protocol) {
-  return (adapters as any)[protocol.module];
-}
