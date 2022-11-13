868d748e1d2658e9f57ebeb0b72c0b6de9a3e57b
ZhangYi <zhangyi.sina@gmail.com>
2022-11-13 20:21:26 +0800

    bank-of-chain's peggedUSD.

---
+++ src/adapters/peggedAssets/bank-of-chain/index.ts
@@ -0,0 +1,50 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  ethereum: {
+    issued: ["0x83131242843257bc6C43771762ba467346Efb2CF"],//USDi
+  }
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks?.[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** decimals, "issued", false);
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  ethereum: {
+    minted: chainMinted("ethereum", 18),
+    unreleased: async () => ({}),
+  },
+};
+
+export default adapter;
