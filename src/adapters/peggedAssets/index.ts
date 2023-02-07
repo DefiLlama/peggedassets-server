110ff702693b0d6af170cea0016188bfd2aa4c6f
0xngmi <80541789+0xngmi@users.noreply.github.com>
2023-02-07 17:15:45 +0000

    Update index.ts

--- src/adapters/peggedAssets/index.ts
+++ src/adapters/peggedAssets/index.ts
@@ -94,7 +94,7 @@ import baousd from "./baousd";
 import pina from "./meme-dollar";
 import djed from "./djed";
 import c98dollar from "./coin98-dollar";
-import usp from "./usp";
+import usp from "./usp-stablecoin";
 
 export default {
   tether,
