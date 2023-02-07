87667e8865b825d5e31d24f0e73b260f65370926
realdealshaman <realdealshaman11@gmail.com>
2023-02-07 01:31:51 +0000

    add usp stable adapter

---
+++ src/adapters/peggedAssets/usp/index.ts
@@ -0,0 +1,52 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  avax: {
+    issued: ["0xdaCDe03d7Ab4D81fEDdc3a20fAA89aBAc9072CE2"],
+  },
+  
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks?.[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** decimals, "issued", false);
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  avax: {
+    minted: chainMinted("avax", 18),
+    unreleased: async () => ({}),
+  },
+  
+};
+
+export default adapter;
