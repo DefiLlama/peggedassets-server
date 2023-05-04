01c099d82ff59ce96dabaee1c8c3ee9f8b5f5803
0xngmi <0xngmi@protonmail.com>
2023-05-04 01:52:08 +0100

    add crv usd adapter

---
+++ src/adapters/peggedAssets/crv-usd/index.ts
@@ -0,0 +1,55 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    const logs = (
+        await sdk.api.util
+          .getLogs({
+            keys: [],
+            toBlock: _ethBlock,
+            chain,
+            target: "0x818709b85052ddc521fae9c78737b27316337e3a",
+            fromBlock: 17182152,
+            topic: 'AddMarket(address,address,address,address,uint256)',
+          })
+      ).output.map((l:any)=>"0x"+l.data.slice(26, 66));
+      for(const log of logs){
+        const totalDebt = (
+            await sdk.api.abi.call({
+            abi: {"stateMutability":"view","type":"function","name":"total_debt","inputs":[],"outputs":[{"name":"","type":"uint256"}]},
+            target: log,
+            block: _ethBlock,
+            chain: chain,
+            })
+        ).output;
+        sumSingleBalance(
+            balances,
+            "peggedUSD",
+            totalDebt / 10 ** decimals,
+            "issued",
+            false
+        );
+    }
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  ethereum: {
+    minted: chainMinted("ethereum", 18),
+    unreleased: async () => ({}),
+  },
+};
+
+export default adapter;
