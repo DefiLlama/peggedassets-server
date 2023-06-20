4b7329942ee6109121bcc5ac8f6c561a321497c6
realdealshaman <realdealshaman11@gmail.com>
2023-06-20 22:27:53 +0100

    add new cash address

--- src/adapters/peggedAssets/stabl-fi/index.ts
+++ src/adapters/peggedAssets/stabl-fi/index.ts
@@ -14,7 +14,10 @@ type ChainContracts = {
 
 const chainContracts: ChainContracts = {
   polygon: {
-    issued: ["0x80487b4f8f70e793a81a42367c225ee0b94315df"],
+    issued: [
+      "0x80487b4f8f70e793a81a42367c225ee0b94315df",
+      "0x5D066D022EDE10eFa2717eD3D79f22F949F8C175"
+  ],
   },
 };
 
