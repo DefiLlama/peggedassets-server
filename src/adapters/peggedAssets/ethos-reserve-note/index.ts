43c09f73133568a8d29c8aabd29318d6dc3d82ec
realdealshaman <realdealshaman11@gmail.com>
2023-07-12 07:19:49 +0100

    update code

--- src/adapters/peggedAssets/ethos-reserve-note/index.ts
+++ src/adapters/peggedAssets/ethos-reserve-note/index.ts
@@ -1,36 +1,54 @@
 const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
 import {
   ChainBlocks,
   PeggedIssuanceAdapter,
+  Balances,
 } from "../peggedAsset.type";
 
-const chainContracts = {
-    optimism: {
-        issued: "0xc5b001dc33727f8f26880b184090d3e252470d45",
-    },
+type ChainContracts = {
+  [chain: string]: {
+    [contract: string]: string[];
+  };
 };
 
-async function optimismMinted() {
-return async function (
+const chainContracts: ChainContracts = {
+  optimism: {
+    issued: ["0xc5b001dc33727f8f26880b184090d3e252470d45"],
+  },
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
     _timestamp: number,
     _ethBlock: number,
     _chainBlocks: ChainBlocks
-) {
-    const totalSupply = (
-      await sdk.api.abi.call({
-        abi: "erc20:totalSupply",
-        target: chainContracts.optimism.issued,
-        block: _ethBlock,
-        chain: "optimism",
-      })
-    ).output;
-    return { ernUSD: totalSupply / 10 ** 18 };
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks?.[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(
+        balances,
+        "peggedUSD",
+        totalSupply / 10 ** decimals,
+        "issued",
+        false
+      );
+    }
+    return balances;
   };
 }
 
 const adapter: PeggedIssuanceAdapter = {
   optimism: {
-    minted: optimismMinted(),
+    minted: chainMinted("optimism", 18),
     unreleased: async () => ({}),
   },
 };
