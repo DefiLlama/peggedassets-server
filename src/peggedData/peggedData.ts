7ea8044035291df9059c2b8ff451bfaf90de118a
realdealshaman <realdealshaman11@gmail.com>
2023-08-08 19:27:45 +0100

    add cg id

--- src/peggedData/peggedData.ts
+++ src/peggedData/peggedData.ts
@@ -2406,8 +2406,8 @@ export default [
     "PayPal USD is designed to maintain a stable $1 USD value. It's backed by dollar deposits, US treasuries, and cash equivalents",
   mintRedeemDescription:
     "As PayPal USD rolls out, users will be able to buy, sell, hold, and transfer it in the PayPal app or on their site",
-  onCoinGecko: false,
-  gecko_id: null,
+  onCoinGecko: "true",
+  gecko_id: "paypal-usd",
   cmcId: null,
   pegType: "peggedUSD",
   pegMechanism: "fiat-backed",
