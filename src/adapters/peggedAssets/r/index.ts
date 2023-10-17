3109c78a945e08dc3dd3dd7295ae29a1ad179f23
define <Lovelovenow@protonmail.com>
2023-10-17 12:45:45 +0100

    add unreleased address to R

--- src/adapters/peggedAssets/r/index.ts
+++ src/adapters/peggedAssets/r/index.ts
@@ -15,6 +15,7 @@ type ChainContracts = {
 const chainContracts: ChainContracts = {
   ethereum: {
     issued: ["0x183015a9bA6fF60230fdEaDc3F43b3D788b13e21"],
+    unreleased: ["0x2ba26baE6dF1153e29813d7f926143f9c94402f3"]
   },
   base: {
     issued: ["0xafb2820316e7bc5ef78d295ab9b8bb2257534576"],
@@ -48,11 +49,36 @@ async function chainMinted(chain: string, decimals: number) {
     return balances;
   };
 }
+async function chainUnreleased(chain: string, decimals: number, owner: string) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const reserve = (
+        await sdk.api.erc20.balanceOf({
+          target: issued,
+          owner: owner,
+          block: _chainBlocks?.[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(balances, "peggedUSD", reserve / 10 ** decimals);
+    }
+    return balances;
+  };
+}
 
 const adapter: PeggedIssuanceAdapter = {
   ethereum: {
     minted: chainMinted("ethereum", 18),
-    unreleased: async () => ({}),
+    unreleased: chainUnreleased(
+      "ethereum",
+      18,
+      chainContracts.ethereum.unreleased[0]
+    ),
   },
   base: {
     minted: chainMinted("base", 18),
