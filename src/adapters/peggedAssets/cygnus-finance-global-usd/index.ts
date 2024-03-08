b99d74d93094f124a1e22737ef88a65d86ce47f3
greatpie <greatpie@live.com>
2024-03-08 20:19:49 +0800

    add cgUSD(pegged with USD)

---
+++ src/adapters/peggedAssets/cygnus-finance-global-usd/index.ts
@@ -0,0 +1,56 @@
+const sdk = require("@defillama/sdk");
+import { sumSingleBalance } from "../helper/generalUtil";
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+  Balances,
+} from "../peggedAsset.type";
+
+type ChainContracts = {
+  [chain: string]: {
+    issued: string;
+    unreleased: string[];
+  };
+};
+
+const chainContracts: ChainContracts = {
+  base: {
+    issued: "0xCa72827a3D211CfD8F6b00Ac98824872b72CAb49",
+    unreleased: [],
+  },
+};
+
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    const totalSupply = (
+      await sdk.api.abi.call({
+        abi: "erc20:totalSupply",
+        target: chainContracts[chain].issued,
+        block: _chainBlocks?.[chain],
+        chain: chain,
+      })
+    ).output;
+    sumSingleBalance(
+      balances,
+      "peggedUSD",
+      totalSupply / 10 ** decimals,
+      "issued",
+      false
+    );
+    return balances;
+  };
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  base: {
+    minted: chainMinted("base", 6),
+    unreleased: async () => ({}),
+  },
+};
+
+export default adapter;
