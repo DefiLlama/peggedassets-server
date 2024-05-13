45c6b50c7104f71f99e47b7a7e616e8b24be3932
Define101 <Lovelovenow@protonmail.com>
2024-05-13 14:48:01 +0100

    Listing bitSmiley USD

--- src/adapters/peggedAssets/index.ts
+++ src/adapters/peggedAssets/index.ts
@@ -187,7 +187,8 @@ import wen from "./magma-wen";
 import gyd from "./gyroscope-gyd";
 import isc from "./international-stable-currency";
 import usc from "./orby-network-usc-stablecoin";
-import fxbtcusd from "./f-x-btc-usd"
+import fxbtcusd from "./f-x-btc-usd";
+import bitusd from "./bitsmiley-bitusd"
 
 export default {
   tether,
@@ -379,5 +380,6 @@ export default {
   "international-stable-currency": isc,
   "orby-network-usc-stablecoin": usc,
   "knox-dollar": knox,
-  "f-x-btc-usd": fxbtcusd
+  "f-x-btc-usd": fxbtcusd,
+  "bitsmiley-bitusd": bitusd
 };
