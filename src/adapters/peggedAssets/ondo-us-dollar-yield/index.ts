825d2f307884ec08317ef803164a34cc9055ad23
Lilian Ludford <lilian@ondo.finance>
2024-05-20 14:27:38 -0400

    lint

--- src/adapters/peggedAssets/ondo-us-dollar-yield/index.ts
+++ src/adapters/peggedAssets/ondo-us-dollar-yield/index.ts
@@ -10,14 +10,18 @@ const chainContracts = {
     unreleased: ["0x94FEC56BBEcEaCC71c9e61623ACE9F8e1B1cf473"],
   },
   sui: {
-    issued: ["0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb"],
+    issued: [
+      "0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb",
+    ],
   },
-  solana:{
-    issued: ["A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6"]
+  solana: {
+    issued: ["A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6"],
   },
   aptos: {
-    issued: ["0xcfea864b32833f157f042618bd845145256b1bf4c0da34a7013b76e42daa53cc"]
-  }
+    issued: [
+      "0xcfea864b32833f157f042618bd845145256b1bf4c0da34a7013b76e42daa53cc",
+    ],
+  },
 };
 
 import { addChainExports } from "../helper/getSupply";
