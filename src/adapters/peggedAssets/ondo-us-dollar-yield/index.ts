829208f10fd282ebbb66ba0c2013f8ebe7893ca5
Lilian Ludford <lilian@ondo.finance>
2024-05-20 15:41:36 -0400

    rm polygon

--- src/adapters/peggedAssets/ondo-us-dollar-yield/index.ts
+++ src/adapters/peggedAssets/ondo-us-dollar-yield/index.ts
@@ -2,9 +2,6 @@ const chainContracts = {
   ethereum: {
     issued: "0x96F6eF951840721AdBF46Ac996b59E0235CB985C",
   },
-  polygon: {
-    issued: "0x96F6eF951840721AdBF46Ac996b59E0235CB985C",
-  },
   mantle: {
     issued: "0x5bE26527e817998A7206475496fDE1E68957c5A6",
     unreleased: ["0x94FEC56BBEcEaCC71c9e61623ACE9F8e1B1cf473"],
