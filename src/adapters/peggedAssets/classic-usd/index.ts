3e8adb6517d8c778d77a308bdcea9331f667950e
leon-do <leondo123@gmail.com>
2024-06-04 16:34:47 -0400

    Add USC to Polygon

--- src/adapters/peggedAssets/classic-usd/index.ts
+++ src/adapters/peggedAssets/classic-usd/index.ts
@@ -1,7 +1,12 @@
 const chainContracts = {
   ethereumclassic: {
     issued: ["0xDE093684c796204224BC081f937aa059D903c52a"],
+    pegType: 'peggedUSD',
   },
+  polygon: {
+    issued: ["0x131409b31bf446737dd04353d43dacada544b6fa"],
+    pegType: 'peggedUSD',
+  }
 };
 
 import { addChainExports } from "../helper/getSupply";
