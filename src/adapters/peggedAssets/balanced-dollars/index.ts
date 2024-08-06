7203ae0ab2c66ed29622e25125cc005ff43220ee
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-08-06 13:50:54 +0300

    fix balanced-dollar

--- src/adapters/peggedAssets/balanced-dollars/index.ts
+++ src/adapters/peggedAssets/balanced-dollars/index.ts
@@ -2,34 +2,34 @@ import { getXSupply, getICONSupply } from "../helper/icon"
 
 const adapter = {
     icon: {
-        minted: getICONSupply()
+        minted: () => getICONSupply()
     },
     // ICON chains where bnUSD is minted natively
     havah: {
-        minted: getXSupply("0x100.icon"),
+        minted: () => getXSupply("0x100.icon"),
     },
     // COSMOS chains where bnUSD is minted natively
     injective: {
-        minted: getXSupply("injective-1"),
+        minted: () => getXSupply("injective-1"),
     },
     archway: {
-        minted: getXSupply("archway-1"),
+        minted: () => getXSupply("archway-1"),
     },
     // EVM chains where bnUSD is minted natively
-    avalanche: {
-        minted: getXSupply("0xa86a.avax"),
+    avax: {
+        minted: () => getXSupply("0xa86a.avax"),
     },
     arbitrum: {
-        minted: getXSupply("0xa4b1.arbitrum"),
+        minted: () => getXSupply("0xa4b1.arbitrum"),
     },
     base: {
-        minted: getXSupply("0x2105.base"),
+        minted: () => getXSupply("0x2105.base"),
     },
     optimism: {
-        minted: getXSupply("0xa.optimism"),
+        minted: () => getXSupply("0xa.optimism"),
     },
     bsc: {
-        minted: getXSupply("0x38.bsc"),
+        minted: () => getXSupply("0x38.bsc"),
     }, 
   };
   
