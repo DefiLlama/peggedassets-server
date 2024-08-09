16a7a177f013c0f144c189f3138ff1867c92a653
realdealshaman <realdealshaman11@gmail.com>
2024-08-09 18:11:40 +0100

    export the unreleased amount

--- src/adapters/peggedAssets/djed/index.ts
+++ src/adapters/peggedAssets/djed/index.ts
@@ -17,20 +17,25 @@ const assetIDs = {
 async function getCardanoSupply() {
   let balances = {} as Balances;
   let supply = await getTotalSupply(assetIDs.cardano.issued[0]);
+  sumSingleBalance(balances, "peggedUSD", supply, "issued", false);
+  return balances;
+}
+
+async function getLockedCardanoSupply() {
+  let balances = {} as Balances;
   const lockedBalance =
     (await getTokenBalance(
       "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344",
       "addr1zx82ru5f7p8ewhhdvahueg2s4gxs3gxl66fkygdekkjs74sm5kjdmrpmng059yellupyvwgay2v0lz6663swmds7hp0q4vpw0l"
     )) / 1e6;
-  supply -= lockedBalance;
-  sumSingleBalance(balances, "peggedUSD", supply, "issued", false);
-
+  sumSingleBalance(balances, "peggedUSD", lockedBalance, "issued", false);
   return balances;
 }
 
 const adapter: PeggedIssuanceAdapter = {
   cardano: {
     minted: getCardanoSupply,
+    unreleased: getLockedCardanoSupply
   },
 };
 
