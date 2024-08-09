c23f5161094e94e7e1ad61c28e41d9139efa9b3c
realdealshaman <realdealshaman11@gmail.com>
2024-08-09 13:52:15 +0100

    update unissued address

--- src/adapters/peggedAssets/djed/index.ts
+++ src/adapters/peggedAssets/djed/index.ts
@@ -20,7 +20,7 @@ async function getCardanoSupply() {
   const lockedBalance =
     (await getTokenBalance(
       "8db269c3ec630e06ae29f74bc39edd1f87c819f1056206e879a1cd61446a65644d6963726f555344",
-      "addr1zxem3j9xw7gyqnry0mfdhku7grrzu0707dc9fs68zwkln5sm5kjdmrpmng059yellupyvwgay2v0lz6663swmds7hp0qul0eqc"
+      "addr1zx82ru5f7p8ewhhdvahueg2s4gxs3gxl66fkygdekkjs74sm5kjdmrpmng059yellupyvwgay2v0lz6663swmds7hp0q4vpw0l"
     )) / 1e6;
   supply -= lockedBalance;
   sumSingleBalance(balances, "peggedUSD", supply, "issued", false);
