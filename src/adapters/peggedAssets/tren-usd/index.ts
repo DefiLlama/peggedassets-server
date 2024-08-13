51fdd2cd553d682a4cd1357d12d606bbf924e79a
rustumatizon <8722069+rustumatizon@users.noreply.github.com>
2024-08-13 12:21:23 -0400

    feat: add tren usd

---
+++ src/adapters/peggedAssets/tren-usd/index.ts
@@ -0,0 +1,30 @@
+const chainContracts = {
+  // mode: {
+  //   issued: ["0x15f74458aE0bFdAA1a96CA1aa779D715Cc1Eefe4"],
+  // },
+  ethereum: {
+    issued: ["0x15f74458aE0bFdAA1a96CA1aa779D715Cc1Eefe4"],
+  },
+  optimism: {
+    issued: ["0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487"],
+  },
+  arbitrum: {
+    issued: ["0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487"],
+  },
+  era: {
+    issued: ["0x5FC44E95eaa48F9eB84Be17bd3aC66B6A82Af709"],
+  },
+  polygon_zkevm: {
+    issued: ["0xCA68ad4EE5c96871EC6C6dac2F714a8437A3Fe66"],
+  },
+  mantle: {
+    issued: ["0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487"],
+  },
+  linea: {
+    issued: ["0x894134a25a5faC1c2C26F1d8fBf05111a3CB9487"],
+  },
+};
+
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts);
+export default adapter;
