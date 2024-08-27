f6caf8b291974665dfc6e681febacbf2fcda4612
tserg <8017125+tserg@users.noreply.github.com>
2024-08-27 22:00:55 +0800

    add opus

---
+++ src/adapters/peggedAssets/opus-cash/abi.ts
@@ -0,0 +1,11 @@
+export const starknetTotalSupplyAbi = {
+  name: "total_supply",
+  type: "function",
+  inputs: [],
+  outputs: [
+    {
+      type: "core::integer::u256",
+    },
+  ],
+  state_mutability: "view",
+};
