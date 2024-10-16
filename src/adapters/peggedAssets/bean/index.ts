90a13f739908a26db85512d88688878f765c007a
Soil King <157099073+soilking@users.noreply.github.com>
2024-10-15 21:20:30 -0700

    simplified

--- src/adapters/peggedAssets/bean/index.ts
+++ src/adapters/peggedAssets/bean/index.ts
@@ -1,32 +1,9 @@
-const sdk = require("@defillama/sdk");;
-import { sumSingleBalance } from "../helper/generalUtil";
-import { ChainBlocks, PeggedIssuanceAdapter } from "../peggedAsset.type";
-
-const BEAN_ERC20 = "0xbea0005b8599265d41256905a9b3073d397812e4";
-
-async function arbitrumMinted(_timestamp: number, arbBlock: number, _chainBlocks: ChainBlocks) {
-
-  const balances = {};
-
-  const totalSupply = (
-    await sdk.api.abi.call({
-      abi: "erc20:totalSupply",
-      target: BEAN_ERC20,
-      block: arbBlock,
-      chain: "arbitrum",
-    })
-  ).output;
-
-  sumSingleBalance(balances, "peggedUSD", totalSupply / 10 ** 6, 'issued', false);
-
-  return balances;
-};
-
-const adapter: PeggedIssuanceAdapter = {
+const chainContracts = {
   arbitrum: {
-    minted: arbitrumMinted,
-    unreleased: async () => ({})
-  }
+    issued: ["0xBEA0005B8599265D41256905A9B3073D397812E4"],
+  },
 };
 
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts);
 export default adapter;
