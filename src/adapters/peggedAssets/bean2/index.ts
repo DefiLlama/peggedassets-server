b514f5f194984434487eb8971056ae12895d670f
Real Shaman <85087525+realdealshaman@users.noreply.github.com>
2024-10-19 17:22:54 +0100

    Revert "Bean token on Arbitrum"

--- src/adapters/peggedAssets/bean2/index.ts
+++ src/adapters/peggedAssets/bean2/index.ts
@@ -1,6 +1,6 @@
 const chainContracts = {
-  arbitrum: {
-    issued: ["0xBEA0005B8599265D41256905A9B3073D397812E4"],
+  ethereum: {
+    issued: ["0xBEA0000029AD1c77D3d5D23Ba2D8893dB9d1Efab"],
   },
 };
 
--- src/adapters/peggedAssets/index.ts
+++ src/adapters/peggedAssets/index.ts
@@ -67,7 +67,7 @@ import fusd from "./fantom-usd";
 import uxd from "./uxd-stablecoin";
 import usdh from "./usdh";
 import fpi from "./frax-price-index";
-import bean from "./bean";
+import bean2 from "./bean2";
 import usdlemma from "./usdlemma";
 //import pandousd from "./pando-usd";
 import dusd from "./digitaldollar";
@@ -282,7 +282,7 @@ export default {
   "uxd-stablecoin": uxd,
   usdh,
   "frax-price-index": fpi,
-  bean: bean,
+  bean: bean2,
   usdlemma,
   //"pando-usd": pandousd,
   digitaldollar: dusd,
