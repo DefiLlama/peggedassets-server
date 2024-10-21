2dc6e926311b6586854e3050788f03b96f0c1922
0xngmi <0xngmi@protonmail.com>
2024-10-21 22:30:49 +0100

    not peggedVAR

--- src/adapters/peggedAssets/bitcoin-usd-btcfi/index.ts
+++ src/adapters/peggedAssets/bitcoin-usd-btcfi/index.ts
@@ -1,12 +1,12 @@
 const chainContracts = {
   bfc: {
-    issued: ["0x6906Ccda405926FC3f04240187dd4fAd5DF6d555"], pegType: 'peggedVAR'
+    issued: ["0x6906Ccda405926FC3f04240187dd4fAd5DF6d555"]
   },
   base: {
-    bridgedFromBfc: ["0xe4b20925d9e9a62f1e492e15a81dc0de62804dd4"], pegType: 'peggedVAR'
+    bridgedFromBfc: ["0xe4b20925d9e9a62f1e492e15a81dc0de62804dd4"]
   },
 };
 
 import { addChainExports } from "../helper/getSupply";
-const adapter = addChainExports(chainContracts, undefined, { pegType: 'peggedVAR' })
+const adapter = addChainExports(chainContracts)
 export default adapter;
