74326c3b3a2d4a9a8d24975769d032f73d875405
Define101 <Lovelovenow@protonmail.com>
2024-11-19 17:16:11 +0000

    fix usds

--- src/adapters/peggedAssets/usds/index.ts
+++ src/adapters/peggedAssets/usds/index.ts
@@ -1,13 +1,19 @@
+import { addChainExports,solanaMintedOrBridged } from "../helper/getSupply";
+import {  PeggedIssuanceAdapter } from "../peggedAsset.type";
+
 const chainContracts = {
     ethereum: {
       issued: ["0xdC035D45d973E3EC169d2276DDab16f1e407384F"],
     },
-    solana: {
-      bridgeOnETH: ["USDSvKzCxbVwSEsu1amJb8BgrUq3c39sGirmT3bmbng"],
-    },
   };
   
   
-  import { addChainExports } from "../helper/getSupply";
-  const adapter = addChainExports(chainContracts);
-  export default adapter;
+  const adapter: PeggedIssuanceAdapter = {
+    ...addChainExports(chainContracts),
+   
+    solana: {
+      ethereum: solanaMintedOrBridged(["USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA"]),
+    },
+  };
+  
+  export default adapter; 
