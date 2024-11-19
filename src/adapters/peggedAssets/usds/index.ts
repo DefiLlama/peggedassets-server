7646166bb21539d864afafaad52f963dfc679b1f
Define101 <Lovelovenow@protonmail.com>
2024-11-19 16:54:08 +0000

    add solana usds

--- src/adapters/peggedAssets/usds/index.ts
+++ src/adapters/peggedAssets/usds/index.ts
@@ -2,6 +2,9 @@ const chainContracts = {
     ethereum: {
       issued: ["0xdC035D45d973E3EC169d2276DDab16f1e407384F"],
     },
+    solana: {
+      bridgeOnETH: ["USDSvKzCxbVwSEsu1amJb8BgrUq3c39sGirmT3bmbng"],
+    },
   };
   
   
