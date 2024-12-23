e14357d40904bdefdd0184bd7811ca8a17df5c35
Vickler Charles <charlesvikler@gmail.com>
2024-12-23 20:35:50 +0100

    add xy

---
+++ src/adapters/peggedAssets/tren-debt-token/index.ts
@@ -0,0 +1,43 @@
+const sdk = require("@defillama/sdk");
+
+import {
+  ChainBlocks,
+  PeggedIssuanceAdapter,
+} from "../peggedAsset.type";
+
+const chainContracts = {
+  arbitrum: {
+    minted: "0xD4fe6e1e37dfCf35E9EEb54D4cca149d1c10239f", // Contrato que emite la stablecoin en Arbitrum
+    curvePool: "0x2b2e23b7c1b0de9040011b860cc575650d0817f7", // Contrato del pool Curve
+  },
+  ethereum: {
+    bridgedFromArbitrum: "0xe9766D6aed0A73255f95ACC1F263156e746B70ba", // Contrato de puente en Ethereum
+  },
+};
+
+async function mintedOnArbitrum() {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    const totalSupply = (
+      await sdk.api.erc20.totalSupply({
+        target: chainContracts.arbitrum.minted,
+        block: _chainBlocks.arbitrum,
+        chain: "arbitrum",
+      })
+    ).output;
+    return { peggedUSD: totalSupply / 10 ** 18 };
+  };
+}
+
+
+
+const adapter: PeggedIssuanceAdapter = {
+  arbitrum: {
+    minted: mintedOnArbitrum(),
+  },
+};
+
+export default adapter;
