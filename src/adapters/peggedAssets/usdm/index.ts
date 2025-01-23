40708428ec319c7338e16de7c8a325ce081e2be9
Guido Peirano <guidopeirano316@gmail.com>
2025-01-23 20:18:15 -0300

    Adding zkSync

--- src/adapters/peggedAssets/usdm/index.ts
+++ src/adapters/peggedAssets/usdm/index.ts
@@ -14,6 +14,9 @@ const chainContracts = {
   arbitrum: {
     issued: "0x59d9356e565ab3a36dd77763fc0d87feaf85508c",
   },
+  zksync: {
+    issued: "0x7715c206A14Ac93Cb1A6c0316A6E5f8aD7c9Dc31",
+  },
 };
 
 
