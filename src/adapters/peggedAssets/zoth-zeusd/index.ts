2f25932dfedf4bf7daaf62ec53d360bb56bcc53d
realdealshaman <realdealshaman11@gmail.com>
2025-01-30 03:58:41 +0000

    include all chains

--- src/adapters/peggedAssets/zoth-zeusd/index.ts
+++ src/adapters/peggedAssets/zoth-zeusd/index.ts
@@ -1,9 +1,19 @@
 const chainContracts = {
+  ethereum: {
+    issued: ["0x7DC9748DA8E762e569F9269f48F69A1a9F8Ea761"],
+  },
   metis: {
-    issued: ["0x2d3D1a6982840Dd88bC2380Fd557F8A9D5e27a77"],
+    bridgedFromETH: ["0x2d3D1a6982840Dd88bC2380Fd557F8A9D5e27a77"], 
+  },
+  manta: {
+    bridgedFromETH: ["0x7DC9748DA8E762e569F9269f48F69A1a9F8Ea761"],
+  },
+  avax: {
+    bridgedFromETH: ["0x7DC9748DA8E762e569F9269f48F69A1a9F8Ea761"],
   },
 };
 
+
 import { addChainExports } from "../helper/getSupply";
-const adapter = addChainExports(chainContracts);
+const adapter = addChainExports(chainContracts, undefined, { decmials: 6});
 export default adapter;
