d1d701b9958e7ddbd6a1dd664b8a7a244e19df82
realdealshaman <realdealshaman11@gmail.com>
2025-02-18 22:52:21 +0000

    add berachain

--- src/adapters/peggedAssets/paypal-usd/index.ts
+++ src/adapters/peggedAssets/paypal-usd/index.ts
@@ -5,8 +5,11 @@ const chainContracts = {
   solana: {
     issued: ["2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo"],
   },
+  berachain: {
+    bridgedFromETH: ["0x688e72142674041f8f6Af4c808a4045cA1D6aC82"],
+  }
 };
 
 import { addChainExports } from "../helper/getSupply";
-const adapter = addChainExports(chainContracts);
+const adapter = addChainExports(chainContracts, undefined, { decmials: 6});
 export default adapter;
