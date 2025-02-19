2a27bdd0619812405f5e1776b83835ffacb744f0
aaron.soh <aaron.soh@wspn.io>
2025-02-19 11:52:30 +0800

    update unreleased address

--- src/adapters/peggedAssets/worldwide-usd/index.ts
+++ src/adapters/peggedAssets/worldwide-usd/index.ts
@@ -1,6 +1,7 @@
 const chainContracts = {
     ethereum: {
       issued: ["0x7Cd017ca5ddb86861FA983a34b5F495C6F898c41"],
+      unreleased: ["0xf3380D451a0cc1990A1DC7b3A97590F4e5a9a6C9"]
     },
     polygon: {
       issued: ["0x7Cd017ca5ddb86861FA983a34b5F495C6F898c41"],
