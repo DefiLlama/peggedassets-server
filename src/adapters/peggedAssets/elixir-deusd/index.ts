068c78edbddc97fc4456bc6a44c064e0510dd950
realdealshaman <realdealshaman11@gmail.com>
2025-03-03 17:29:06 +0000

    add sei's fastusd

--- src/adapters/peggedAssets/elixir-deusd/index.ts
+++ src/adapters/peggedAssets/elixir-deusd/index.ts
@@ -1,9 +1,10 @@
 const chainContracts = {
     ethereum: {
-      issued: [
-        "0x15700B564Ca08D9439C58cA5053166E8317aa138",
-      ],
+      issued: ["0x15700B564Ca08D9439C58cA5053166E8317aa138"],
     },
+    sei: {
+      bridgedFromETH: ["0x37a4dd9ced2b19cfe8fac251cd727b5787e45269"]
+    }
   };
   
   import { addChainExports } from "../helper/getSupply";
