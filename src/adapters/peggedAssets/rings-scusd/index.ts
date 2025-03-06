be5748873942d867909b7627d3649a69b2bbb3ba
realdealshaman <realdealshaman11@gmail.com>
2025-03-06 13:39:16 +0000

    remove comment

--- src/adapters/peggedAssets/rings-scusd/index.ts
+++ src/adapters/peggedAssets/rings-scusd/index.ts
@@ -3,7 +3,7 @@ const chainContracts = {
   sonic: {
     issued: ["0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE"],
   },
-}; // flagged as double counted since the tokens used on Ethereum to ming scUSD on Sonic remain on Ethereum used in various defi projects via the veda boringvault
+}; 
 
 import { addChainExports } from "../helper/getSupply";
 const adapter = addChainExports(chainContracts);
