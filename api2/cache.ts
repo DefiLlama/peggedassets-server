e78bc8327ad0e2dfc069a0cf1c02b2c225f25cee
realshaman <realdealshaman11@gmail.com>
2025-04-25 18:37:09 -0600

    bump cache

--- api2/cache.ts
+++ api2/cache.ts
@@ -27,7 +27,7 @@ export const cache: {
   historicalPrices?: DailyPeggedPrices[]
 } = {}
 
-const cacheFile = 'stablecoin-cache-v3'
+const cacheFile = 'stablecoin-cache-v4'
 
 export async function initCache(cacheType = CacheType.API_SERVER) {
   console.time('Cache initialized')
