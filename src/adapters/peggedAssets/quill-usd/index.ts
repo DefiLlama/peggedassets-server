7498f82fef801fc709634c2cf4cab646656528aa
realshaman <realdealshaman11@gmail.com>
2025-05-15 18:00:18 -0600

    add new address

--- src/adapters/peggedAssets/quill-usd/index.ts
+++ src/adapters/peggedAssets/quill-usd/index.ts
@@ -1,6 +1,9 @@
 const chainContracts = {
   scroll: {
-    issued: ["0x6F2A1A886Dbf8E36C4fa9F25a517861A930fBF3A"],
+    issued: [
+      "0x6F2A1A886Dbf8E36C4fa9F25a517861A930fBF3A", // old address
+      "0xdb9e8f82d6d45fff803161f2a5f75543972b229a" // new address
+    ],
   }
 };
 
