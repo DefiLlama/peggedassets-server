741f3c8705623abfe453fdcd0c29f5dc41752bde
Sergey Maslov <maslovsa@gmail.com>
2025-05-20 16:36:17 +0400

    Add wA7A5

--- src/adapters/peggedAssets/a7a5/index.ts
+++ src/adapters/peggedAssets/a7a5/index.ts
@@ -1,13 +1,13 @@
-const pegType = "peggedRUB";
 const chainContracts = {
   ethereum: {
-    issued: "0x6fA0BE17e4beA2fCfA22ef89BF8ac9aab0AB0fc9", pegType
+    issued: ["0x6fA0BE17e4beA2fCfA22ef89BF8ac9aab0AB0fc9", // A7A5 with rebase
+            "0x0d57436f2d39c0664c6f0f2e349229483f87ea38"], // wA7A5 no rebase
   },
   tron: {
-    issued: "TLeVfrdym8RoJreJ23dAGyfJDygRtiWKBZ", pegType
+    issued: ["TLeVfrdym8RoJreJ23dAGyfJDygRtiWKBZ"],
   },
 };
 
 import { addChainExports } from "../helper/getSupply";
-const adapter = addChainExports(chainContracts, undefined, { pegType });
+const adapter = addChainExports(chainContracts, undefined, { pegType: "peggedRUB" });
 export default adapter;
