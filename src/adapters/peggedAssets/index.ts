624930f7a7bd8b275ac3da2de0cd2809ec91ce0e
realshaman <realdealshaman11@gmail.com>
2025-05-19 22:04:52 -0600

    add adapter for yala yu

--- src/adapters/peggedAssets/index.ts
+++ src/adapters/peggedAssets/index.ts
@@ -263,6 +263,7 @@ import xbankusde from './xbanking-usde';
 import orkiusdk from './orki-usd';
 import plumeUsd from './plume-usd';
 import mead2 from './mead-2';
+import yu from './yala-yu';
 
 export default {
   tether,
@@ -531,4 +532,5 @@ export default {
   "xbanking-usde": xbankusde,
   "orki-usd": orkiusdk,
   "mead-2": mead2,
+  "yala-yu": yu,
 };
