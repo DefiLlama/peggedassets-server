2dc0421a3c21ac2b82c2f0f994b72cf9088a29dc
realshaman <realdealshaman11@gmail.com>
2025-07-02 13:33:25 -0600

    add chains

--- src/adapters/peggedAssets/m-by-m^0/index.ts
+++ src/adapters/peggedAssets/m-by-m^0/index.ts
@@ -1,10 +1,26 @@
-import { addChainExports } from "../helper/getSupply";
+import { addChainExports, solanaMintedOrBridged } from "../helper/getSupply";
 
 const chainContracts = {
     ethereum: {
         issued: ["0x866A2BF4E572CbcF37D5071A7a58503Bfb36be1b"],
     },
+    arbitrum: {
+        bridgedFromEthereum: ["0x866A2BF4E572CbcF37D5071A7a58503Bfb36be1b"],
+    },
+    optimism: {
+        bridgedFromEthereum: ["0x866A2BF4E572CbcF37D5071A7a58503Bfb36be1b"],
+    },
+    plume_mainnet: {
+        bridgedFromEthereum: ["0x866A2BF4E572CbcF37D5071A7a58503Bfb36be1b"],
+    },
+    hyperliquid: {
+        bridgedFromEthereum: ["0x866A2BF4E572CbcF37D5071A7a58503Bfb36be1b"],
+    },
 };
 
-const adapter = addChainExports(chainContracts);
+const adapter = addChainExports(chainContracts, {}, { decimals: 6 });
+adapter.solana = {
+    ethereum: solanaMintedOrBridged(["mzerokyEX9TNDoK4o2YZQBDmMzjokAeN6M2g2S3pLJo"])
+};
+
 export default adapter;
