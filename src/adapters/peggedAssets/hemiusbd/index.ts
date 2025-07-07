2067e5ccf1a8d26ac48e52eaff1c80d330f34793
0xminer11 <arali.naveen1@gmail.com>
2025-07-07 13:05:53 +0530

    added hemiUSBD

---
+++ src/adapters/peggedAssets/hemiusbd/index.ts
@@ -0,0 +1,42 @@
+import { ChainBlocks, PeggedIssuanceAdapter } from "../peggedAsset.type";
+const sdk = require("@defillama/sdk");
+
+type Chain = "hemi"
+
+const chainContracts: Partial<Record<Chain, string>> = {
+  hemi:"0x71E7c8F2B7D7F6c99E375023916CB3ed9ffC4621"
+};
+
+async function minted(chain: Chain) {
+  return async function (
+    // @ts-ignore
+    timestamp: number,
+    block: number,
+    // @ts-ignore
+    chainBlocks: ChainBlocks
+  ) {
+    const totalSupply = (
+      await sdk.api.abi.call({
+        abi: "erc20:totalSupply",
+        target: chainContracts[chain],
+        block,
+        chain,
+      })
+    ).output;
+
+    return { peggedUSD: totalSupply / 10 ** 18 };
+  };
+}
+
+
+const adapter: PeggedIssuanceAdapter = Object.keys(chainContracts).reduce(
+  (acc, item) => ({
+    ...acc,
+    [item]: {
+      minted: minted(item as Chain)
+    },
+  }),
+  {}
+);
+
+export default adapter;
