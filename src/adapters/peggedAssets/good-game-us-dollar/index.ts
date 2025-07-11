55c891e71fef36e3b0cf07d4b0d7814d39299ccf
realshaman <realdealshaman11@gmail.com>
2025-07-11 15:48:43 -0600

    fix adapter

--- src/adapters/peggedAssets/good-game-us-dollar/index.ts
+++ src/adapters/peggedAssets/good-game-us-dollar/index.ts
@@ -1,22 +1,59 @@
+const sdk = require("@defillama/sdk");
 import { PeggedIssuanceAdapter } from "../peggedAsset.type";
 import {
   bridgedSupply,
   solanaMintedOrBridged,
 } from "../helper/getSupply";
+import {
+  sumSingleBalance,
+} from "../helper/generalUtil";
+import {
+  ChainBlocks,
+  Balances,
+} from "../peggedAsset.type";
 
-const chainContracts: any = {
-  evm: ["0xFFFFFF9936BD58a008855b0812B44D2c8dffE2aA"],
+const chainContracts: { [chain: string]: string[] } = {
+  polygon: ["0xFFFFFF9936BD58a008855b0812B44D2c8dffE2aA"],
+  bsc: ["0xFFFFFF9936BD58a008855b0812B44D2c8dffE2aA"],
   solana: ["GGUSDyBUPFg5RrgWwqEqhXoha85iYGs6cL57SyK4G2Y7"],
 };
 
 const decimals = 6;
 
+async function chainMinted(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain]) {
+      const totalSupply = (
+        await sdk.api.abi.call({
+          abi: "erc20:totalSupply",
+          target: issued,
+          block: _chainBlocks?.[chain],
+          chain: chain,
+        })
+      ).output;
+      sumSingleBalance(
+        balances,
+        "peggedUSD",
+        totalSupply / 10 ** decimals,
+        "issued",
+        false
+      );
+    }
+    return balances;
+  };
+}
+
 const adapter: PeggedIssuanceAdapter = {
   polygon: {
-    minted: bridgedSupply("polygon", decimals, chainContracts.evm),
+    minted: chainMinted("polygon", decimals),
   },
   bsc: {
-    polygon: bridgedSupply("bsc", decimals, chainContracts.evm),
+    polygon: bridgedSupply("bsc", decimals, chainContracts.bsc),
   },
   solana: {
     polygon: solanaMintedOrBridged(chainContracts.solana),
