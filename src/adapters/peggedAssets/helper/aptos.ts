de62bce4be230cfcefdf4ba320987d4625e37c47
realshaman <realdealshaman11@gmail.com>
2025-07-16 14:00:39 -0600

    handle BENJI totalsupply

--- src/adapters/peggedAssets/helper/aptos.ts
+++ src/adapters/peggedAssets/helper/aptos.ts
@@ -36,6 +36,18 @@ export async function getTokenSupply(token: string) {
     const coinInfo = data.find((coin: any) => coin.type === '0x4de5876d8a8e2be7af6af9f3ca94d9e4fafb24b5f4a5848078d8eb08f08e808a::ds_token::TokenData')
     return coinInfo.data.total_issued / 1e6
   }
+  
+  // Handle Franklin Onchain U.S. Government Money Fund token
+  if (token === '0x7b5e9cac3433e9202f28527f707c89e1e47b19de2c33e4db9521a63ad219b739') {
+    const concurrentSupply = data.find((coin: any) => coin.type === '0x1::fungible_asset::ConcurrentSupply');
+    const metadata = data.find((coin: any) => coin.type === '0x1::fungible_asset::Metadata');
+    if (concurrentSupply && metadata) {
+      const supply = concurrentSupply.data.current.value;
+      const decimals = metadata.data.decimals;
+      return parseInt(supply) / 10 ** decimals;
+    }
+  }
+  
   const coinInfo = data.find((coin: any) => coin.type.startsWith('0x1::coin::CoinInfo'));
 
   return coinInfo.data.supply.vec[0].integer.vec[0].value / 10 ** coinInfo.data.decimals;
