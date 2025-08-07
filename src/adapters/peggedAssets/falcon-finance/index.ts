54e7cd745206eaf926141f9426050addff31c84a
rinfalcon <rin@falcon.finance>
2025-08-07 19:15:06 +1000

    feat: add BSC for USDf

--- src/adapters/peggedAssets/falcon-finance/index.ts
+++ src/adapters/peggedAssets/falcon-finance/index.ts
@@ -5,6 +5,9 @@ const chainContracts = {
   ethereum: {
     issued: ["0xFa2B947eEc368f42195f24F36d2aF29f7c24CeC2"],
   },
+  bsc: {
+    bridgedFromETH: "0xb3b02E4A9Fb2bD28CC2ff97B0aB3F6B3Ec1eE9D2",
+  },
 };
 
 const adapter: PeggedIssuanceAdapter = {
