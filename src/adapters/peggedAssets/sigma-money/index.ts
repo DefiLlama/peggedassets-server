475737ba336c674c29d304084384ec6a6262fe1c
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-08-20 15:19:11 +0200

    track https://sigma.money/

---
+++ src/adapters/peggedAssets/sigma-money/index.ts
@@ -0,0 +1,11 @@
+const chainContracts = {
+  bsc: {
+    issued: [
+      "0x5519a479Da8Ce3Af7f373c16f14870BbeaFDa265",
+    ],
+  },
+};
+
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts);
+export default adapter;
