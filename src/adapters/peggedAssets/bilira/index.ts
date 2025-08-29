176abf10e4d6a5d2fc52c7c54ad33e225c56b8af
realshaman <realdealshaman11@gmail.com>
2025-08-28 18:21:14 -0600

    add pegged type and unreleased

--- src/adapters/peggedAssets/bilira/index.ts
+++ src/adapters/peggedAssets/bilira/index.ts
@@ -1,22 +1,26 @@
+const pegType = "peggedTRY";
+
 const chainContracts = {
     ethereum: {
-        issued: ["0x2c537e5624e4af88a7ae4060c022609376c8d0eb"],
+        issued: ["0x2c537e5624e4af88a7ae4060c022609376c8d0eb"], 
+        unreleased: ["0xd03846601b6b77965693741aAF296491Bc10A0EB"], 
+        pegType
     },
     base: {
-        issued: ["0xfb8718a69aed7726afb3f04d2bd4bfde1bdcb294"],
+        issued: ["0xfb8718a69aed7726afb3f04d2bd4bfde1bdcb294"], pegType
     },
     avax: {
-        issued: ["0x564a341df6c126f90cf3ecb92120fd7190acb401"],
+        issued: ["0x564a341df6c126f90cf3ecb92120fd7190acb401"], pegType
     },
     bsc: {
-        issued: ["0xc1fdbed7dac39cae2ccc0748f7a80dc446f6a594"],
+        issued: ["0xc1fdbed7dac39cae2ccc0748f7a80dc446f6a594"], pegType
     },
     polygon: {
-        issued: ["0x4fb71290ac171e1d144f7221d882becac7196eb5"],
+        issued: ["0x4fb71290ac171e1d144f7221d882becac7196eb5"], pegType
     }
   };
   
 import { isSea } from "node:sea";
   import { addChainExports } from "../helper/getSupply";
-  const adapter = addChainExports(chainContracts);
+  const adapter = addChainExports(chainContracts, undefined, { pegType });
   export default adapter;
