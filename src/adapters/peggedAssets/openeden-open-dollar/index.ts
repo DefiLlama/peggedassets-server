df68ffa1fd51d83950b76fd7658e12cc05a00daa
Danny_Plasma <shs@plasma.to>
2025-09-23 13:07:50 +0100

    Add Plasma USDe and USDO

--- src/adapters/peggedAssets/openeden-open-dollar/index.ts
+++ src/adapters/peggedAssets/openeden-open-dollar/index.ts
@@ -5,6 +5,9 @@ const chainContracts = {
   base: {
     issued: ["0xaD55aebc9b8c03FC43cd9f62260391c13c23e7c0"],
   },
+  plasma: {
+    issued: ["0x87e617C7484aDE79FcD90db58BEB82B057facb48"],
+  },
   bsc: {
     issued: ["0x302e52AFf9815B9D1682473DBFB9C74F9B750AA8"],
   },
