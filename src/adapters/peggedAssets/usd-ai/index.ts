c7bb94862e1a62b17e0b622813d746a909f69d85
realshaman <realdealshaman11@gmail.com>
2025-09-29 10:05:23 -0600

    add all chains

--- src/adapters/peggedAssets/usd-ai/index.ts
+++ src/adapters/peggedAssets/usd-ai/index.ts
@@ -1,9 +1,15 @@
 const chainContracts = {
-    plasma: {
+    arbitrum: {
       issued: ["0x0A1a1A107E45b7Ced86833863f482BC5f4ed82EF"],
-    }
+    },
+    plasma: {
+      bridgedFromArb: ["0x0A1a1A107E45b7Ced86833863f482BC5f4ed82EF"],
+    },
+    ethereum: {
+      bridgedFromArb: ["0x0A1a1A107E45b7Ced86833863f482BC5f4ed82EF"],
+    },
   };
-  
+
   import { addChainExports } from "../helper/getSupply";
   const adapter = addChainExports(chainContracts);
   export default adapter;
