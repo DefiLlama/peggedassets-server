d26ba4bac27318108b72235e654ba865a32a475a
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-09-30 14:18:22 +0200

    Update gusd (#605)
    * update gusd (unreleased)
    
    * fix calculation

--- src/adapters/peggedAssets/gusd/index.ts
+++ src/adapters/peggedAssets/gusd/index.ts
@@ -1,12 +1,45 @@
-import { addChainExports } from '../helper/getSupply'
-import { PeggedIssuanceAdapter } from '../peggedAsset.type'
+import { ChainApi } from "@defillama/sdk";
+import { sumSingleBalance } from "../helper/generalUtil";
+import { getApi } from "../helper/getSupply";
+import { Balances, PeggedIssuanceAdapter } from "../peggedAsset.type";
+const axios = require("axios");
 
-const chainContracts = {
-  ethereum: {
-    issued: ['0xaf6186b3521b60e27396b5d23b48abc34bf585c5'], // GUSD
+const API_URL = 'https://www.gate.com/apim/v3/earn/staking/product?coin=GUSD'
+
+const issued = '0xaf6186b3521b60e27396b5d23b48abc34bf585c5'
+
+const chainMinted = (chain: string, decimals: number) => {
+  return async (_api: ChainApi) => {
+    const api = await getApi(chain, _api)
+    const balances: Balances = {}
+    const totalSupply = await api.call({ abi: "erc20:totalSupply", target: issued })
+    sumSingleBalance(balances, "peggedUSD", Number(totalSupply) / 10 ** decimals, "issued", false)
+    return balances
   }
 }
 
-const adapter: PeggedIssuanceAdapter = addChainExports(chainContracts)
+const unreleased = async (chain: string, decimals: number) => {
+  return async (_api: ChainApi) => {
+    const api = await getApi(chain, _api)
+    const balances: Balances = {}
+
+    const [totalSupply, { data }] = await Promise.all([
+      api.call({ abi: "erc20:totalSupply", target: issued }),
+      axios.get(API_URL)
+    ])
+
+    const amount = Math.round(data?.data?.hold_amount_total) 
+    sumSingleBalance(balances, "peggedUSD", (Number(totalSupply) / 10 ** decimals) - amount , "issued", false)
+    return balances
+
+  }
+}
+
+const adapter: PeggedIssuanceAdapter = {
+  ethereum: {
+    minted: chainMinted('ethereum', 6),
+    unreleased: unreleased('ethereum', 6)
+  }
+}
 
 export default adapter
