ada1396f2a6591b9bb90a68ad5164aa9bae186c1
Yash <mathuryash0@gmail.com>
2025-10-15 15:23:56 +0530

    add pusd

---
+++ src/adapters/peggedAssets/pusd/index.ts
@@ -0,0 +1,9 @@
+const chainContracts = {
+    ethereum: {
+      issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+    }
+  };
+  
+  import { addChainExports } from "../helper/getSupply";
+  const adapter = addChainExports(chainContracts);
+  export default adapter;
