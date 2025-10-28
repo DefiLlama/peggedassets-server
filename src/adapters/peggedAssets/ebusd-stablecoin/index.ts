672ed6c9ed9f5938b9628e878e022a04bdac7624
realshaman <realdealshaman11@gmail.com>
2025-10-27 22:51:33 -0600

    add plasma to ebusd

--- src/adapters/peggedAssets/ebusd-stablecoin/index.ts
+++ src/adapters/peggedAssets/ebusd-stablecoin/index.ts
@@ -2,6 +2,9 @@ const chainContracts = {
     ethereum: {
       issued: ["0x09fD37d9AA613789c517e76DF1c53aEce2b60Df4"],
     },
+    plasma: {
+      issued: ["0xef7b1a03e0897c33b63159e38d779e3970c0e2fc"],
+    },
   };
   
   
