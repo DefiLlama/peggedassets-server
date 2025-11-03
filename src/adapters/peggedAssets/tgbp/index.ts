21f85b0684bb1909f2c6738307a13384fabe6569
joshreyes93 <joshua@smartrmail.com>
2025-11-03 21:33:19 +0000

    Fix GBP peg on tGBP

--- src/adapters/peggedAssets/tgbp/index.ts
+++ src/adapters/peggedAssets/tgbp/index.ts
@@ -1,24 +1,30 @@
 const chainContracts = {
     ethereum: {
       issued: ["0x27f6c8289550fCE67f6B50BeD1F519966aFE5287"],
+      pegType: 'peggedGBP',
     },
     bsc: {
       issued: ["0x27f6c8289550fCE67f6B50BeD1F519966aFE5287"],
+      pegType: 'peggedGBP',
     },
     base: {
       issued: ["0x27f6c8289550fCE67f6B50BeD1F519966aFE5287"],
+      pegType: 'peggedGBP',
     },
     polygon: {
       issued: ["0x27f6c8289550fCE67f6B50BeD1F519966aFE5287"],
+      pegType: 'peggedGBP',
     },
     avax: {
       issued: ["0x27f6c8289550fCE67f6B50BeD1F519966aFE5287"],
+      pegType: 'peggedGBP',
     },
     solana: {
       issued: ["2zMqyX4AYCk6mgy5UZ2S7zUaLxwERhK5WjqDzkPPbSpW"],
+      pegType: 'peggedGBP',
     },
   };
   
   import { addChainExports } from "../helper/getSupply";
-  const adapter = addChainExports(chainContracts, undefined, { pegType: "peggedGBP" });
+  const adapter = addChainExports(chainContracts);
   export default adapter;
