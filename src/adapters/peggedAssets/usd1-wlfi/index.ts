1c7b812e81562a300e631d63206a7aee8607fa6b
Rachit Agarwal <76098066+rachitag112@users.noreply.github.com>
2025-11-07 00:29:28 +0530

    Added USD1 on Plume Mainnet

--- src/adapters/peggedAssets/usd1-wlfi/index.ts
+++ src/adapters/peggedAssets/usd1-wlfi/index.ts
@@ -14,6 +14,9 @@ const chainContracts = {
   aptos: {
     issued: ["0x05fabd1b12e39967a3c24e91b7b8f67719a6dacee74f3c8b9fb7d93e855437d2"],
   },
+  plume-mainnet: {
+    issued: ["0x111111d2bf19e43C34263401e0CAd979eD1cdb61"],
+  },
 };
 
 import { addChainExports } from "../helper/getSupply";
