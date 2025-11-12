0a2a3a01b732f33d6a01970ba41a4e42a6c3059b
Yash <mathuryash0@gmail.com>
2025-11-12 18:06:03 +0530

    added binance address for PUSD

--- src/adapters/peggedAssets/pusd/index.ts
+++ src/adapters/peggedAssets/pusd/index.ts
@@ -1,7 +1,10 @@
 const chainContracts = {
     ethereum: {
       issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
-    }
+    },
+    binance: {
+        issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+    },
   };
   
   import { addChainExports } from "../helper/getSupply";
