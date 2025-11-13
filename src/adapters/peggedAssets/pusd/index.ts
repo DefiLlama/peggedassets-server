1deaf6414c1d8a92fdc5bd35b38623efd0e705e8
Yash <mathuryash0@gmail.com>
2025-11-13 22:52:48 +0530

    added bsc and tron for PUSD

--- src/adapters/peggedAssets/pusd/index.ts
+++ src/adapters/peggedAssets/pusd/index.ts
@@ -5,6 +5,9 @@ const chainContracts = {
     binance: {
         issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
     },
+    tron: {
+        issued: ["TF39FD5YwW63mtB1zr9gpVdyFUx1icac2y"]
+    }
   };
   
   import { addChainExports } from "../helper/getSupply";
