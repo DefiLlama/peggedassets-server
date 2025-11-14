6013f7d4f4bb88a7fe087fafbf9411392ee381fd
realshaman <realdealshaman11@gmail.com>
2025-11-14 16:33:13 -0600

    add bsc and tron

--- src/adapters/peggedAssets/palm-usd/index.ts
+++ src/adapters/peggedAssets/palm-usd/index.ts
@@ -1,7 +1,13 @@
 const chainContracts = {
     ethereum: {
       issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
-    }
+    },
+    bsc: {
+      issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+  },
+    tron: {
+      issued: ["TF39FD5YwW63mtB1zr9gpVdyFUx1icac2y"]
+  }
   };
 
   import { addChainExports } from "../helper/getSupply";
