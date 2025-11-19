d910b9f9d49bdec106fadaa12b0c8ce94c60b759
fromstupig <tnphong.tech@gmail.com>
2025-11-20 02:32:09 +0700

    feat: remove dUSD on ronin

--- src/adapters/peggedAssets/dtrinity-usd/index.ts
+++ src/adapters/peggedAssets/dtrinity-usd/index.ts
@@ -5,9 +5,6 @@ const chainContracts = {
   sonic: {
     issued: "0x53a6aBb52B2F968fA80dF6A894e4f1b1020DA975",
   },
-  ronin: {
-    issued: "0x0043a403ada6b63045112d7e979a057c82714fe7",
-  },
   katana: {
     issued: "0xcA52d08737E6Af8763a2bF6034B3B03868f24DDA",
   }
