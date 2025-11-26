20bfd582baac3314d75b794b5be9e0dfb8392d2c
0xpeluche <0xpeluche@proton.me>
2025-11-26 15:18:06 +0100

    Fix: USN

--- src/adapters/peggedAssets/usn/index.ts
+++ src/adapters/peggedAssets/usn/index.ts
@@ -12,6 +12,7 @@ import { bridgedSupply } from "../helper/getSupply";
 const chainContracts: ChainContracts = {
   near: {
     issued: ["usn"],
+    unreleased: ["usn-burn.near"]
   },
   aurora: {
     bridgedFromNear: ["0x5183e1B1091804BC2602586919E6880ac1cf2896"],
@@ -40,9 +41,32 @@ async function chainMinted(chain: string, decimals: number) {
   };
 }
 
+async function chainUnreleased(chain: string, decimals: number) {
+  return async function (
+    _timestamp: number,
+    _ethBlock: number,
+    _chainBlocks: ChainBlocks
+  ) {
+    let balances = {} as Balances;
+    for (let issued of chainContracts[chain].issued) {
+      const totalSupply = await call(issued, "ft_balance_of", { account_id: chainContracts[chain].unreleased[0] });
+
+      sumSingleBalance(
+        balances,
+        "peggedUSD",
+        totalSupply / 10 ** decimals,
+        "issued",
+        false
+      );
+    }
+    return balances;
+  };
+}
+// usn-burn.near
 const adapter: PeggedIssuanceAdapter = {
   near: {
     minted: chainMinted("near", 18),
+    unreleased: chainUnreleased("near", 18)
   },
   aurora: {
     near: bridgedSupply("aurora", 18, chainContracts.aurora.bridgedFromNear),
