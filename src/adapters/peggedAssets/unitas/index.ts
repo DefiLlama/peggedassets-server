a34f4c498e7fa8dff02ad8335006447775e697ba
realshaman <realdealshaman11@gmail.com>
2025-12-03 12:01:06 -0600

    fix to show as bridged from solana

--- src/adapters/peggedAssets/unitas/index.ts
+++ src/adapters/peggedAssets/unitas/index.ts
@@ -1,18 +1,19 @@
 import {PeggedIssuanceAdapter} from "../peggedAsset.type";
-import {addChainExports, solanaMintedOrBridged} from "../helper/getSupply";
+import {addChainExports, solanaMintedOrBridged, bridgedSupply} from "../helper/getSupply";
 
 const chainContracts = {
     bsc: {
-        bridgedFromETH: "0xeA953eA6634d55dAC6697C436B1e81A679Db5882",
+        bridgedFromSol: "0xeA953eA6634d55dAC6697C436B1e81A679Db5882",
     },
 }
 
 const adapter: PeggedIssuanceAdapter = {
-    ...addChainExports(chainContracts),
+    bsc: {
+        solana: bridgedSupply("bsc", 18, [chainContracts.bsc.bridgedFromSol]),
+    },
 
     solana: {
         issued: solanaMintedOrBridged(["9ckR7pPPvyPadACDTzLwK2ZAEeUJ3qGSnzPs8bVaHrSy",]),
     },
 };
-
 export default adapter;
