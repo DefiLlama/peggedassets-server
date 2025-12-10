9ded0927946ec9b6f7d1ca1c0785aee036276340
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2025-12-10 18:00:07 +0100

    fixed palm usd

--- src/adapters/peggedAssets/palm-usd/index.ts
+++ src/adapters/peggedAssets/palm-usd/index.ts
@@ -1,15 +1,16 @@
 const chainContracts = {
-    ethereum: {
-      issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
-    },
-    bsc: {
-      issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+  ethereum: {
+    issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+    reserves: ["0x4b3974aaabdc251b3086ae4a8163110d766c88c8"],
   },
-    tron: {
-      issued: ["TF39FD5YwW63mtB1zr9gpVdyFUx1icac2y"]
+  bsc: {
+    issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
+  },
+  tron: {
+    issued: ["TF39FD5YwW63mtB1zr9gpVdyFUx1icac2y"]
   }
-  };
+};
 
-  import { addChainExports } from "../helper/getSupply";
-  const adapter = addChainExports(chainContracts);
-  export default adapter;
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts);
+export default adapter;
