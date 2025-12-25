884b0f0b74855ec42632b8dd8c4b46d7951185b6
Yash <mathuryash0@gmail.com>
2025-12-25 23:35:31 +0530

    Updated PUSD addresses

--- src/adapters/peggedAssets/palm-usd/index.ts
+++ src/adapters/peggedAssets/palm-usd/index.ts
@@ -1,7 +1,7 @@
 const chainContracts = {
   ethereum: {
     issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
-    reserves: ["0x4b3974aaabdc251b3086ae4a8163110d766c88c8", "0x1760bFB0f8461Fcf4c6768a82436840d23F40252", "0xa1B2A2dFe6300D3f2174e0a4f073Fc0F78F5169F"],
+    reserves: ["0x4b3974aaabdc251b3086ae4a8163110d766c88c8"],
   },
   bsc: {
     issued: ["0xFAF0cEe6B20e2Aaa4B80748a6AF4CD89609a3d78"],
