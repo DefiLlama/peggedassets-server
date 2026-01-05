ed4bf7c8792a2646784f088542efd21fb45cfcaa
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-01-05 23:01:44 +0100

    track jupusd

--- src/adapters/peggedAssets/bilira/index.ts
+++ src/adapters/peggedAssets/bilira/index.ts
@@ -2,8 +2,8 @@ const pegType = "peggedTRY";
 
 const chainContracts = {
     ethereum: {
-        issued: ["0x2c537e5624e4af88a7ae4060c022609376c8d0eb"], 
-        unreleased: ["0xd03846601b6b77965693741aAF296491Bc10A0EB"], 
+        issued: ["0x2c537e5624e4af88a7ae4060c022609376c8d0eb"],
+        unreleased: ["0xd03846601b6b77965693741aAF296491Bc10A0EB"],
         pegType
     },
     base: {
@@ -24,9 +24,8 @@ const chainContracts = {
     plasma: {
         issued: ["0x90729a45948c3078890Bc80F2a4e7870A2Ea4C5E"], pegType
     }
-  };
-  
-import { isSea } from "node:sea";
-  import { addChainExports } from "../helper/getSupply";
-  const adapter = addChainExports(chainContracts, undefined, { pegType });
-  export default adapter;
+};
+
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts, undefined, { pegType });
+export default adapter;
