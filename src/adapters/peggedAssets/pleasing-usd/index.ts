28a188de22f22419498879a9eb691f46a6d5d338
bomi-papa <dirk.hsu@pleasing-international.com>
2026-01-22 22:35:32 +0800

    Update index.ts

--- src/adapters/peggedAssets/pleasing-usd/index.ts
+++ src/adapters/peggedAssets/pleasing-usd/index.ts
@@ -1,1 +1,8 @@
-
+const chainContracts = {
+  arbitrum: {
+    issued: ["0xc8fb643d18f1e53698cfda5c8fdf0cdc03c1dbec"],
+  },
+};
+import { addChainExports } from "../helper/getSupply";
+const adapter = addChainExports(chainContracts);
+export default adapter;
