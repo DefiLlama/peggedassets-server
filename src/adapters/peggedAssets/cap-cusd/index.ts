9e9b90c3d9585ae2b7b9f8004ed5aff1b233eb2b
B̾R̾E̾A̾D̾ <97642356+0xBreadguy@users.noreply.github.com>
2026-01-26 09:10:59 -0800

    Add megaeth chain to pegged assets adapter

--- src/adapters/peggedAssets/cap-cusd/index.ts
+++ src/adapters/peggedAssets/cap-cusd/index.ts
@@ -2,6 +2,9 @@ const chainContracts = {
     ethereum: {
       issued: ["0xcCcc62962d17b8914c62D74FfB843d73B2a3cccC"],
     },
+    megaeth: {
+      issued: ["0xcCcc62962d17b8914c62D74FfB843d73B2a3cccC"],
+    },
   };
   
   import { addChainExports } from "../helper/getSupply";
