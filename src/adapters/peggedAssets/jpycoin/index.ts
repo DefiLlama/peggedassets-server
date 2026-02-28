23de3eb2129688f2aeb5ae375d463c397d721b79
realshaman <realdealshaman11@gmail.com>
2026-02-27 20:37:18 -0600

    update jpyc adapter and add listing

--- src/adapters/peggedAssets/jpycoin/index.ts
+++ src/adapters/peggedAssets/jpycoin/index.ts
@@ -2,21 +2,21 @@
 const chainContracts = {
     ethereum: {
       issued: ["0xe7c3d8c9a439fede00d2600032d5db0be71c3c29"],
-      unreleased: [
-        "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
-      ],
+      // unreleased: [
+      //   "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
+      // ],
     },
     avax: {
       issued: ["0xe7c3d8c9a439fede00d2600032d5db0be71c3c29"],
-      unreleased: [
-        "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
-      ],
+      // unreleased: [
+      //   "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
+      // ],
     },
     polygon: {
       issued: ["0xe7c3d8c9a439fede00d2600032d5db0be71c3c29"],
-      unreleased: [
-        "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
-      ],
+      // unreleased: [
+      //   "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
+      // ],
     },
   }
   
