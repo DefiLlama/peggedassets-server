617682151a2472b5925395f4633b2b106d96552f
Vukašin Manojlović <vmanojlovic@monad.foundation>
2026-03-09 12:28:56 +0100

    Add Monad for rwaUSDi

--- src/adapters/peggedAssets/rwausdi/index.ts
+++ src/adapters/peggedAssets/rwausdi/index.ts
@@ -1,6 +1,9 @@
 const chainContracts = {
     ethereum: {
       issued: ["0xa39986f96b80d04e8d7aeaaf47175f47c23fd0f4"],
+    },
+    monad: {
+      issued: ["0x650b616b46fF94000Eb115926aB8393B90788D76"],
     }
   };
   
