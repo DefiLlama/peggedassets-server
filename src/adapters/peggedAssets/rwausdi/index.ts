f92de5a0c28a8c489d043faacf7c4a629dfedaf8
realshaman <realdealshaman11@gmail.com>
2026-03-27 11:42:53 -0600

    add arbitrum

--- src/adapters/peggedAssets/rwausdi/index.ts
+++ src/adapters/peggedAssets/rwausdi/index.ts
@@ -8,6 +8,9 @@ const chainContracts = {
     base: {
       issued: ["0xd74FB32112b1eF5b4C428Fead8dA8d85A0019009"],
     },
+    arbitrum: {
+      issued: ["0xa39986f96b80d04e8d7aeaaf47175f47c23fd0f4"],
+    },
   };
   
   import { addChainExports } from "../helper/getSupply";
