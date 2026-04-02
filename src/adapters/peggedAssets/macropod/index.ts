d668f52b325042c9f504927bf3f0f42077fc008d
realshaman <realdealshaman11@gmail.com>
2026-04-02 10:47:22 -0600

    add new chains

--- src/adapters/peggedAssets/macropod/index.ts
+++ src/adapters/peggedAssets/macropod/index.ts
@@ -2,6 +2,12 @@ const chainContracts: ChainContracts = {
   rbn: {
     issued: ["0x081599E4936D12c46Bd48913B2329115Cd26cbdd"],
   },
+  ethereum: {
+    issued: ["0x081599E4936D12c46Bd48913B2329115Cd26cbdd"],
+  },
+  solana: {
+    issued: ["CiYXBwHPrdNkMtxR8YEWKv78K6bQjFoEWhPQrZqEmubi"],
+  },
 };
 import { addChainExports } from "../helper/getSupply";
 import { ChainContracts } from "../peggedAsset.type";
