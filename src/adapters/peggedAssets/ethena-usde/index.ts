9e98ce479c2604268f9bbc4bf60c451ee1ba7b2a
realshaman <realdealshaman11@gmail.com>
2026-05-11 16:48:51 -0600

    fix bridge info

--- src/adapters/peggedAssets/ethena-usde/index.ts
+++ src/adapters/peggedAssets/ethena-usde/index.ts
@@ -9,7 +9,7 @@ import { Balances } from "../peggedAsset.type";
    const resp = await function_view({
      functionStr: '0x1::fungible_asset::supply',
      type_arguments: ['0x1::object::ObjectCore'],
-     args: [chainContracts.move.bridgedFromETH[0]],
+     args: [chainContracts.move.issued[0]],
    });
    balances["peggedUSD"] = Number(resp.vec[0]) / 1e6;
  
@@ -21,7 +21,7 @@ const chainContracts = {
     issued: ["0x4c9EDD5852cd905f086C759E8383e09bff1E68B3"],
   },
   move: {
-    bridgedFromETH: "0x9d146a4c9472a7e7b0dbc72da0eafb02b54173a956ef22a9fba29756f8661c6c",
+    issued: ["0x9d146a4c9472a7e7b0dbc72da0eafb02b54173a956ef22a9fba29756f8661c6c"],
   },
   tempo: {
     bridgedFromETH: ["0x20c0000000000000000000002f52d5cc21a3207b"], // USDe on Tempo Mainnet (Stargate Hydra OFT, decimals=6)
@@ -35,7 +35,7 @@ const adapter: PeggedIssuanceAdapter = {
     ethereum: solanaMintedOrBridged(["DEkqHyPN7GMRJ5cArtQFAWefqbZb33Hyf6s5iCwjEonT"]),
   },
   move: {
-    ethereum: moveSupply,
+    minted: moveSupply,
   },
   ton: {
     ethereum: tonTokenSupply("EQAIb6KmdfdDR7CN1GBqVJuP25iCnLKCvBlJ07Evuu2dzP5f"),
