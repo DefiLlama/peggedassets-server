10f9dfd54799fc2ffb02c1754df0ba6e9cdfb622
realshaman <realdealshaman11@gmail.com>
2026-06-17 17:39:53 -0600

    add unreleased team tokens

--- src/adapters/peggedAssets/hyperliquid-native-stablecoin/index.ts
+++ src/adapters/peggedAssets/hyperliquid-native-stablecoin/index.ts
@@ -1,6 +1,10 @@
 const chainContracts = {
     hyperliquid: {
       issued: ["0x111111a1a0667d36bd57c0a9f569b98057111111"],
+      unreleased: [
+        "0x4c2c0f0bb2631b02ac9299c59690914ee7a200b8",
+        "0xc5c21723fdd9e74fd853dd19c8dded71c3767cc2",
+      ],
     },
   };
   
