48584377a7def91bab58280a8638817d3e56f91c
Vukašin Manojlović <vmanojlovic@monad.foundation>
2026-07-02 12:08:10 +0200

    Add GHO on Monad (#831)

--- src/adapters/peggedAssets/gho/index.ts
+++ src/adapters/peggedAssets/gho/index.ts
@@ -2,6 +2,9 @@ const chainContracts = {
   ethereum: {
     issued: ["0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f"],
   },
+  monad: {
+    bridgedFromETH: ["0xfc421aD3C883Bf9E7C4f42dE845C4e4405799e73"],
+  }
 };
 
 import { addChainExports } from "../helper/getSupply";
