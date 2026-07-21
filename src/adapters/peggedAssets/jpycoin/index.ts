0ef58117bd779e90798e0cdb71e170a415dd62d9
realshaman <realdealshaman11@gmail.com>
2026-07-21 00:16:52 -0600

    add kaia

--- src/adapters/peggedAssets/jpycoin/index.ts
+++ src/adapters/peggedAssets/jpycoin/index.ts
@@ -18,6 +18,9 @@ const chainContracts = {
       //   "0x8549E82239a88f463ab6E55Ad1895b629a00Def3",
       // ],
     },
+    klaytn: {
+      issued: ["0xe7c3d8c9a439fede00d2600032d5db0be71c3c29"],
+    },
   }
   
   import { addChainExports } from "../helper/getSupply";
