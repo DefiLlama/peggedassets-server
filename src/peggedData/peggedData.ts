8e488a594c1841b824841d53715cc7848d6c47a4
realshaman <realdealshaman11@gmail.com>
2026-08-18 09:50:43 -0600

    update listing

--- src/peggedData/peggedData.ts
+++ src/peggedData/peggedData.ts
@@ -13043,10 +13043,10 @@ export default [
           issued: ["0x8Fcd23142047A3073ed332a0Ed07d1e8D2BD5177"],
         },
         ink: {
-          issued: ["0x2A66Bb2dA3AD1c854E79307F64b862DECD860D4c"],
+          bridgedFromETH: ["0x2A66Bb2dA3AD1c854E79307F64b862DECD860D4c"],
         },
         base: {
-          issued: ["0x272Ec977f4575df41cD47b1b254954E1C7972789"],
+          bridgedFromETH: ["0x272Ec977f4575df41cD47b1b254954E1C7972789"],
         },
       },
     },
